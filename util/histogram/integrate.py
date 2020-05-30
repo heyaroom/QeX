17eaaed55be1ccff2f955e207b2c9a6110de89b0
heyaroom <42564522+heyaroom@users.noreply.github.com>
2020-05-31 02:05:01 +0900

    Update integrate.py

--- util/histogram/integrate.py
+++ util/histogram/integrate.py
@@ -2,7 +2,7 @@ import re
 
 def expect_pauli(pauli, histogram):
     remove_idx = find_identity(pauli)
-    expected_value = integrate_histogram(histogram, remove_idx)
+    expected_value = integrate_histogram(remove_idx, histogram)
     return expected_value
 
 def find_identity(pauli):
